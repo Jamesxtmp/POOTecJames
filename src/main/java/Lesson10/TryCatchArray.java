d65160d209d0795ac3a83db50f253200dcb30bbf
CCJ18 <CCJ18@DESKTOP-S6M2L1J>
2023-04-29 09:44:27 -0600

    Lesoon 10

---
+++ src/main/java/Lesson10/TryCatchArray.java
@@ -0,0 +1,20 @@
+package Lesson10;
+
+public class TryCatchArray {
+
+    public static void main(String[] args) {
+        try {
+            int numbers[] = {5, 6, 7};
+            System.out.println(numbers[0]);
+            System.out.println(numbers[1]);
+            System.out.println(numbers[2]);
+            System.out.println(numbers[3]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }finally{
+            System.out.println(":)");
+        }
+
+    }
+
+}
